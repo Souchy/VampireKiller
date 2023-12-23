1f62787b62ce716f4949195c435aea6c1c0fc446
Souchy <robyn.g@hotmail.fr>
2023-12-22 22:28:19 -0500

    Fps counter

--- projects/Layer 3 - Presentation/Glaceon/scenes/ui/UiGame.cs
+++ projects/Layer 3 - Presentation/Glaceon/scenes/ui/UiGame.cs
@@ -1,18 +1,23 @@
 using Godot;
+using Godot.Sharp.Extras;
 using System;
 
 public partial class UiGame : Control
 {
-	
+	[NodePath]
+	public Label LblFps { get; set; }
 	public UiSlotActive slot { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.OnReady();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var fps = Engine.GetFramesPerSecond();
+		LblFps.Text = "fps: " + fps.ToString();
 	}
 }
