8a37ae4126a71a2a00c33558c130d96a6984d368
Souchy <robyn.g@hotmail.fr>
2023-12-23 23:04:28 -0500

    Status stats & skill Duration

--- projects/Layer 0 - Domain/Eevee/vampirekiller.eevee/status/Status.cs
+++ projects/Layer 0 - Domain/Eevee/vampirekiller.eevee/status/Status.cs
@@ -31,3 +31,48 @@ public class Status : Identifiable, IStatementContainer
         throw new NotImplementedException();
     }
 }
+
+/// <summary>
+/// Time at which the status duration was created or refreshed 
+/// The time of expiration = StatusDate + StatusDuration.
+/// Used  for status/fx/proj
+/// </summary>
+//public class DurationStartDate : StatDate { }
+///// <summary>
+///// Actual current duration, calculated from TotalDuration and input here through the CreateStatusScript
+///// Used  for status/fx/proj
+///// Must be less than StatusMaxDuration.
+///// </summary>
+//public class CurrentDuration : StatDouble { }
+/// <summary>
+/// Time at which the status/fx/proj must end.
+/// Can get refreshed
+/// </summary>
+public class SkillExpirationDate : StatDate { }
+/// <summary>
+/// Max duration set by the status model.
+/// 0 or null means infinite.
+/// </summary>
+public class SkillMaxDuration : StatDouble { }
+/// <summary>
+/// Current Status stacks
+/// </summary>
+public class StatusStacks : StatInt { }
+/// <summary>
+/// Max stacks set by the status model.
+/// </summary>
+public class StatsMaxStacks : StatInt { }
+
+
+/// <summary>
+/// Base duration for statuses, fxnodes, projectilenodes...
+/// </summary>
+public class BaseDuration : StatDouble { }
+/// <summary>
+/// Increases skill durations
+/// </summary>
+public class IncreasedDuration : StatDouble { }
+/// <summary>
+/// Calculate increased duration to set into a status/fx/proj's SkillExpirationDate
+/// </summary>
+public class TotalDuration : StatDoubleTotal<BaseDuration, IncreasedDuration> { }
